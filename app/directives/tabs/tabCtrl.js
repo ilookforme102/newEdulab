3d23734aa5b7fcd029888c493b9d727693be48b0
ILOOKFORME102\ilookforme102 <t4casa00@students.oamk.fi>
2016-11-22 01:12:16 +0200

    add drag and drop for pick up student in groups

--- app/directives/tabs/tabCtrl.js
+++ app/directives/tabs/tabCtrl.js
@@ -311,49 +311,46 @@ myApp.controller('tab6Ctrl', function($scope) {
     }
 });
 myApp.controller("tab4Ctrl", function($scope) {
+    $scope.numberTeam =
+    $scope.maxItem = 5;
+    $scope.models = {
+        selected: null,
+        max: $scope.maxItem,
+        lists: {
+            "A": [
+                {label: "Item A" },
+                {label: "Item B" },
+                {label: "Item C" },
+                {label: "Item D" }
+                ],
+            "B": [
+                {label: "Item F" },
+                {label: "Item F" },
+                {label: "Item G" },
+                {label: "Item H" }
 
-    $scope.dragoverCallback = function(event, index, external, type) {
-        $scope.logListEvent('dragged over', event, index, external, type);
-        // Disallow dropping in the third row. Could also be done with dnd-disable-if.
-        return index < 10;
+
+    ],
+            "C": [
+
+
+
+            ]
+        },
+        lists2:[
+
+            {label: "Item A" },
+            {label: "Item A" }
+        ]
     };
 
-    $scope.dropCallback = function(event, index, item, external, type, allowedType) {
-        $scope.logListEvent('dropped at', event, index, external, type);
-        if (external) {
-            if (allowedType === 'itemType' && !item.label) return false;
-            if (allowedType === 'containerType' && !angular.isArray(item)) return false;
-        }
-        return item;
-    };
+    // Generate initial model
 
-    $scope.logEvent = function(message, event) {
-        console.log(message, '(triggered by the following', event.type, 'event)');
-        console.log(event);
-    };
 
-    $scope.logListEvent = function(action, event, index, external, type) {
-        var message = external ? 'External ' : '';
-        message += type + ' element is ' + action + ' position ' + index;
-        $scope.logEvent(message, event);
-    };
-
-    $scope.model = [];
-
-    // Initialize model
-    var id = 10;
-    for (var i = 0; i < 3; ++i) {
-        $scope.model.push([]);
-        for (var j = 0; j < 2; ++j) {
-            $scope.model[i].push([]);
-            for (var k = 0; k < 7; ++k) {
-                $scope.model[i][j].push({label: 'Item ' + id++});
-            }
-        }
-    }
-
-    $scope.$watch('model', function(model) {
+    // Model to JSON for demo purpose
+    $scope.$watch('models', function(model) {
         $scope.modelAsJson = angular.toJson(model, true);
     }, true);
+
 
 });
